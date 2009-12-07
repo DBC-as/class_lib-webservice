bc4963ac1be53ca101c66fc4611ca8075b6a433c
pjo <pjo@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2009-12-07 07:39:15 +0000

    new version
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@7238 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- cache_client_class.php
+++ cache_client_class.php
@@ -1,107 +1,41 @@
 <?php
-define(ENDCODE,"zzz");
-class cache_client
-{
-  // to avoid abuse of cache_client
-  private function __construct(){}
 
-  public static function set($obj)
-  {
-    $obj = array("cmd"=>"set","cache"=>$obj); 
-    $ret=self::send($obj);
-
-    return $ret;
-  }
-
-  public static function get($key)
-  {
-    $obj = array("cmd"=>"get","key"=>$key);
+/**
    
-    if( $ret=self::send($obj) )
-      return $ret->data;
+   wrapper for memcache class.
 
-    return false;  
-  }
+*/
 
-  public static function dump()
-  {
-    $obj = array("cmd"=>"dump");
-    if( $response = self::send($obj) )
-      return $response;
+define( HOST,"localhost" );
+define( PORT,"11211" );
 
-    return false;
-  }
-
-  public static function kill()
-  {
-    $obj = array("cmd"=>"kill");
-    if( !self::send($obj) )
-      return false;
-  }
-
-  private function send($obj)
-  {
-    if( ! $socket = self::get_socket() )
-      return false;
-    
-    $write = json_encode($obj).ENDCODE;
-    fwrite($socket,$write);
-    
-    if( ! $response=self::read($socket) )
-      return false;
+class cache
+{
   
-    fclose($socket);         
-    
-    if( $resp = json_decode($response) )
-      return $resp;
-    else
-      return $response;   
-  }
-
-  private static function get_socket($domain='127.0.0.1', $port=5000)
+  private static $_memcache=null;
+  // private constructor to avoid abuse( new() ) of class
+  private function __construct(){}
+  
+  public static function client()
   {
-    if( $socket = fsockopen($domain,  $port) )
-      return $socket;
-
-    return false;
-  }
-
- 
-  private static function read($socket)
-  {    
-    while(  $buf = fread($socket, 1024))
+    if( self::$_memcache==null )
       {
-       if( substr($buf,-3)==ENDCODE )
-	 {
-	   $ret.=substr($buf,0,strlen($buf)-3);
-	   break;
-	 }
-
-        $ret.=$buf;      
+	self::$_memcache=new Memcache();
+	self::$_memcache->connect(HOST,PORT);
       }
-
-     if( $ret == "" || $ret == "na" )
-       return false;
-
-    return $ret;
+    return self::$_memcache;
   }
 
+  public function get($key)
+  {
+    return self::$_memcache->get("mykey");
+  }
+
+  public function set($key,$data)
+  {
+    self::$_memcache->set("mykey", "testhest", false, 600);
+  }
 }
 
 
-class cache_object
-{
-  public $key;
-  public $time_to_live;
-  public $timestamp;
-  public $data;
-
-  public function __construct($key,$data,$time_to_live=5)
-  {
-    $this->key=$key;
-    $this->time_to_live=$time_to_live;
-    $this->timestamp=time();
-    $this->data=$data;    
-  } 
- }
 ?>
