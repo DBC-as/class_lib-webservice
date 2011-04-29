ba2dd5abff43cb8845cb64caefad36ca792c11e1
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2011-04-29 12:46:59 +0000

    PID
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@29921 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- verbose_class.php
+++ verbose_class.php
@@ -25,36 +25,41 @@
  *
  * Usage: \n
  * verbose::open(logfile_name, log_mask); \n
- * verbose::log(FATAL,"could not find value x")\n
+ * verbose::log(FATAL,'could not find value x')\n
  *  
  * Example:
- * verbose::open("my_trace_file.log", "WARNING+FATAL+TIMER"); \n
- * verbose::log(FATAL, "Cannot find database");\n
+ * verbose::open('my_trace_file.log', 'WARNING+FATAL+TIMER'); \n
+ * verbose::log(FATAL, 'Cannot find database');\n
  * 
  * Example:
- * verbose::open("my_trace_file.log", WARNING+FATAL+TIMER); \n
- * verbose::log(FATAL, "Cannot find database");\n
+ * verbose::open('my_trace_file.log', WARNING+FATAL+TIMER); \n
+ * verbose::log(FATAL, 'Cannot find database');\n
+ *  
+ * Example (will add process-id to all verbose lines):
+ * verbose::open('my_trace_file.log', PID+WARNING+FATAL+TIMER); \n
+ * verbose::log(FATAL, 'Cannot find database');\n
  *  
  * Example:
- * verbose::open("my_trace_file.log", 77, "H:i:s d:m:y"); \n
- * verbose::log(TRACE, "db::look_up_user()");\n
+ * verbose::open('my_trace_file.log', 77, 'H:i:s d:m:y'); \n
+ * verbose::log(TRACE, 'db::look_up_user()');\n
  */
 
-@ define("WARNING",0x01);
-@ define("ERROR",0x02);
-@ define("FATAL",0x04);
-@ define("STAT",0x08);
-@ define("TIMER",0x10);
-@ define("DEBUG",0x20);
-@ define("TRACE",0x40);
-@ define("Z3950",0x80);
-@ define("OCI",0x100);
+@ define('WARNING',0x01);
+@ define('ERROR',0x02);
+@ define('FATAL',0x04);
+@ define('STAT',0x08);
+@ define('TIMER',0x10);
+@ define('DEBUG',0x20);
+@ define('TRACE',0x40);
+@ define('Z3950',0x80);
+@ define('OCI',0x100);
 
 class verbose {
 
   static $verbose_file_name;
   static $verbose_mask;
   static $date_format;
+  static $my_pid = '';
 
   private function __construct() {}
   private function __destruct() {}
@@ -66,15 +71,17 @@ class verbose {
   * @param verbose_mask (string or integer)
   **/
 
-  function open($verbose_file_name, $verbose_mask, $date_format="") {
+  function open($verbose_file_name, $verbose_mask, $date_format='') {
     if (!self::$date_format = $date_format)
-      self::$date_format="H:i:s-d/m/y";
+      self::$date_format='H:i:s-d/m/y';
     self::$verbose_file_name=$verbose_file_name;
     if (!is_string($verbose_mask))
       self::$verbose_mask=(empty($verbose_mask) ? 0 : $verbose_mask);
     else
-      foreach (explode('+', $verbose_mask) as $vm)
+      foreach (explode('+', $verbose_mask) as $vm) {
         if (defined(trim($vm))) self::$verbose_mask |= constant(trim($vm));
+        if ($vm == 'PID') self::$my_pid = ' [' . getmypid() . ']';
+      }
   }
 
  /**
@@ -86,24 +93,24 @@ class verbose {
   function log($verbose_level, $str) {
     if (self::$verbose_file_name && $verbose_level & self::$verbose_mask) {
       switch ($verbose_level) {
-        case WARNING : $vtext = "WARNING"; break;
-        case ERROR :   $vtext = "ERROR"; break;
-        case FATAL :   $vtext = "FATAL"; break;
-        case STAT :    $vtext = "STAT"; break;
-        case TIMER :   $vtext = "TIMER"; break;
-        case DEBUG :   $vtext = "DEBUG"; break;
-        case TRACE :   $vtext = "TRACE"; break;
-        case Z3950 :   $vtext = "Z3950"; break;
-        case OCI :     $vtext = "OCI"; break;
-        default :      $vtext = "UNKNOWN"; break;
+        case WARNING : $vtext = 'WARNING'; break;
+        case ERROR :   $vtext = 'ERROR'; break;
+        case FATAL :   $vtext = 'FATAL'; break;
+        case STAT :    $vtext = 'STAT'; break;
+        case TIMER :   $vtext = 'TIMER'; break;
+        case DEBUG :   $vtext = 'DEBUG'; break;
+        case TRACE :   $vtext = 'TRACE'; break;
+        case Z3950 :   $vtext = 'Z3950'; break;
+        case OCI :     $vtext = 'OCI'; break;
+        default :      $vtext = 'UNKNOWN'; break;
       }
 
-      if ($fp = @ fopen(self::$verbose_file_name,"a")) {
+      if ($fp = @ fopen(self::$verbose_file_name,'a')) {
         if(!ereg("\n\$", $str)) $str .= "\n";
-        fwrite($fp, $vtext . " " . date(self::$date_format) . ": " . $str);
+        fwrite($fp, $vtext . self::$my_pid . ' ' . date(self::$date_format) . ': ' . $str);
         fclose($fp);
       } else
-        die("FATAL: Cannot open " . self::$verbose_file_name);
+        die('FATAL: Cannot open ' . self::$verbose_file_name);
     }
   }
 }
