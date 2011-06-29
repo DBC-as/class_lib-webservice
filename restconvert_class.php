84c54d137e3470971da97c29e376c1991303a330
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2011-06-29 10:56:55 +0000

    htmlspecialchars()
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@30913 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- restconvert_class.php
+++ restconvert_class.php
@@ -23,65 +23,65 @@
 
 class restconvert {
 
-  //private $charset = "ISO-8859-1";
-  private $charset = "utf-8";
-	private $soap_header;
-	private $soap_footer;
-	private $default_namespace = "";
+    //private $charset = "ISO-8859-1";
+    private $charset = "utf-8";
+    private $soap_header;
+    private $soap_footer;
+    private $default_namespace = "";
 
-	public function __construct($namespace="") {
-		$this->soap_header='<?xml version="1.0" encoding="%s"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"%s><SOAP-ENV:Body>';
-		$this->soap_footer='</SOAP-ENV:Body></SOAP-ENV:Envelope>';
-    if ($namespace)
-      $this->default_namespace = ' xmlns="' . $namespace . '"';
-	}
+    public function __construct($namespace="") {
+        $this->soap_header='<?xml version="1.0" encoding="%s"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"%s><SOAP-ENV:Body>';
+        $this->soap_footer='</SOAP-ENV:Body></SOAP-ENV:Envelope>';
+        if ($namespace)
+            $this->default_namespace = ' xmlns="' . $namespace . '"';
+    }
 
-	/** \brief Transform REST parameters to SOAP-request
-	 *
- 	*/
-	public function rest2soap(&$config) {
- 	 $soap_actions = $config->get_value("soapAction", "setup");
- 	 $action_pars = $config->get_value("action", "rest");
-   if (!$all_actions = $action_pars["ALL"]) $all_actions = array();
- 	 if (is_array($soap_actions) && is_array($action_pars) 
-    && $_GET["action"]
-    && $soap_actions[$_GET["action"]] && $action_pars[$_GET["action"]]) {
-     if ($_GET["charset"]) $this->charset = $_GET["charset"];
- 	   if ($node_value = $this->build_xml(array_merge($all_actions, &$action_pars[$_GET["action"]]), 
-                                        explode("&", $_SERVER["QUERY_STRING"]))) {
- 	     return sprintf($this->soap_header, $this->charset, $this->default_namespace) .
-              $this->rest_tag_me($soap_actions[$_GET["action"]], $node_value) .
-              $this->soap_footer;
-			} 
- 	 }
-	}
+    /** \brief Transform REST parameters to SOAP-request
+     *
+     */
+    public function rest2soap(&$config) {
+        $soap_actions = $config->get_value("soapAction", "setup");
+        $action_pars = $config->get_value("action", "rest");
+        if (!$all_actions = $action_pars["ALL"]) $all_actions = array();
+        if (is_array($soap_actions) && is_array($action_pars)
+                && $_GET["action"]
+                && $soap_actions[$_GET["action"]] && $action_pars[$_GET["action"]]) {
+            if ($_GET["charset"]) $this->charset = $_GET["charset"];
+            if ($node_value = $this->build_xml(array_merge($all_actions, &$action_pars[$_GET["action"]]),
+                                               explode("&", $_SERVER["QUERY_STRING"]))) {
+                return sprintf($this->soap_header, $this->charset, $this->default_namespace) .
+                       $this->rest_tag_me($soap_actions[$_GET["action"]], $node_value) .
+                       $this->soap_footer;
+            }
+        }
+    }
 
-	private function build_xml($action, $query) {
- 	 foreach ($action as $key => $tag) {
- 	   if (is_array($tag)) {
- 	     $ret .= $this->rest_tag_me($key, $this->build_xml($tag, $query));
- 	   } else {
- 	     foreach ($query as $parval) {
- 	       list($par, $val) = $this->par_split($parval);
- 	       if ($tag == $par) $ret .= $this->rest_tag_me($tag, $val);
- 	     }
- 	   }
-		}
- 	 return $ret;
-	}
+    private function build_xml($action, $query) {
+        foreach ($action as $key => $tag) {
+            if (is_array($tag)) {
+                $ret .= $this->rest_tag_me($key, $this->build_xml($tag, $query));
+            } else {
+                foreach ($query as $parval) {
+                    list($par, $val) = $this->par_split($parval);
+                    if ($tag == $par) $ret .= $this->rest_tag_me($tag, htmlspecialchars($val));
+                }
+            }
+        }
+        return $ret;
+    }
 
-	private function par_split($parval) {
- 	 list($par, $val) = explode("=", $parval, 2);
- 	 return array(preg_replace("/\[[^]]*\]/", "", urldecode($par)), urldecode($val));
-	}
+    private function par_split($parval) {
+        list($par, $val) = explode("=", $parval, 2);
+        return array(preg_replace("/\[[^]]*\]/", "", urldecode($par)), urldecode($val));
+    }
 
-	function rest_tag_me($tag, $val) {
-    if (empty($val)) return;
+    function rest_tag_me($tag, $val) {
+        if (empty($val)) return;
 
-    if ($i = strrpos($tag, "."))
-      $tag = substr($tag, $i+1);
-	  return "<$tag>$val</$tag>";
-	}
+        if ($i = strrpos($tag, "."))
+            $tag = substr($tag, $i+1);
+        return "<$tag>$val</$tag>";
+    }
 }
 
 ?>
