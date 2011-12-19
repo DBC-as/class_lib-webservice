3a9cb84b3a89f5a0065c67fa5d302e0febd4bc8b
mkr <mkr@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2011-12-19 13:36:50 +0000

    Renamed class
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@36325 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- WebServiceClientHelper_class.php
+++ WebServiceClientHelper_class.php
@@ -3,7 +3,7 @@ require_once("xmlconvert_class.php");
 require_once("objconvert_class.php");
 require_once("curl_class.php");
 
-class ws_testimplementation {
+class WebserviceClientHelper {
 
 	private $xml_request_path;
 	private $request_objects;
@@ -16,17 +16,17 @@ class ws_testimplementation {
 		$this->objconvert = new objconvert();
 	}
 
-	function load_request($request_name) {
+	public function load_request($request_name) {
 		$request=file_get_contents($this->xml_request_path.$request_name.'.xml');
 		$obj=$this->xmlconvert->soap2obj($request);
 		$this->request_objects[$request_name]=$obj;
 	}
 
-	function print_request($request_name) {
-		echo $xml=$this->objconvert->obj2xml($this->request_objects[$request_name]);
+	public function get_request($request_name) {
+		return $xml=$this->objconvert->obj2xml($this->request_objects[$request_name]);
 	}
 
-	function change_tag_value(&$request_object, $target_tag_name, $target_tag_value) {
+	public function change_tag_value(&$request_object, $target_tag_name, $target_tag_value) {
 		foreach ($request_object as $k=>$v) {
 			if($k==$target_tag_name) {
 				$v->_value=$target_tag_value;
@@ -38,12 +38,20 @@ class ws_testimplementation {
 		}
 	}
 
-	function get_request_object($request_name) {
+	public function get_request_object($request_name) {
 		return $this->request_objects[$request_name];
 	}
 
-	function  show_request_objects() {
+	public function  show_request_objects() {
 		print_r($this->request_objects);
+	}
+
+	public function send_request($request_name, $request_action) {
+		$curl = new curl(); 
+		$curl->set_timeout(30);
+		$xml=$this->get_request($request_name);
+		$curl->set_post_xml($xml);  
+		return $res = $curl->get($request_action);
 	}
 }
 
