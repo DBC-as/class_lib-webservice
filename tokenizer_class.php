ac12c6b1cb6cbc382363aef7eff61a510e631188
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2012-01-19 10:19:28 +0000

    astyle --style=kr -s2 -S -y
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@37420 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- tokenizer_class.php
+++ tokenizer_class.php
@@ -20,7 +20,7 @@
 */
 
 
-/** 
+/**
 *
 *
 * $t=new tokenizer();
@@ -32,155 +32,163 @@
 
 class tokenizer {
 
-	/// Token <string>
+  /// Token <string>
   var $token;
 
-	/// Expression to split by in preg format <string>
+  /// Expression to split by in preg format <string>
   var $split_expression = '';
 
-	/// List of operators <array>
+  /// List of operators <array>
   var $operators=array();
-	/// List of indexes <array>
+  /// List of indexes <array>
   var $indexes=array();
-	/// List of ignores <array>
+  /// List of ignores <array>
   var $ignore=array();
-	/// Prefix for operator <array>
-	var $index_prefixes=array();
+  /// Prefix for operator <array>
+  var $index_prefixes=array();
 
-	/// List of tokens <array>
-	var $tokenlist=array();
+  /// List of tokens <array>
+  var $tokenlist=array();
 
-	/// Sets weather operators and indexes are case insensitive <bool>
-	var $case_insensitive=FALSE;
+  /// Sets weather operators and indexes are case insensitive <bool>
+  var $case_insensitive=FALSE;
 
   // indexes which shold be searched as raw
   var $raw_index=array();
 
- /** \brief Check if token is operator.
-  *
-  * @param token (string)
-  * @return (bool)
-  *
-  */
+  /** \brief Check if token is operator.
+   *
+   * @param token (string)
+   * @return (bool)
+   *
+   */
 
   function is_operator($token) {
-			if($this->case_insensitive) {
-				$token=strtolower($token);
-			}
+    if ($this->case_insensitive) {
+      $token=strtolower($token);
+    }
 
-      if(in_array($token,$this->operators)) {
-        return TRUE;
-      }
+    if (in_array($token,$this->operators)) {
+      return TRUE;
+    }
     return FALSE;
   }
 
- /** \brief Check if token is index.
-  *
-  * @param token (string)
-  * @return (bool)
-  *
-  */
+  /** \brief Check if token is index.
+   *
+   * @param token (string)
+   * @return (bool)
+   *
+   */
 
   function is_index($token) {
 
-			if($this->case_insensitive) {
-				$token=strtolower($token);
-			}
+    if ($this->case_insensitive) {
+      $token=strtolower($token);
+    }
 
-      if(in_array($token,$this->indexes)) {
+    if (in_array($token,$this->indexes)) {
+      return TRUE;
+    }
+
+    foreach($this->index_prefixes as $v) {
+      if (in_array(str_replace("$v".'.','',$token), $this->indexes)) {
         return TRUE;
       }
-
-      foreach($this->index_prefixes as $v) {
-      	if(in_array(str_replace("$v".'.','',$token), $this->indexes)) {
-          return TRUE;
-        }
-      }
+    }
 
     return FALSE;
   }
 
   function is_raw_index($idx) {
     foreach ($this->raw_index as $i)
-      if (substr($idx, 0, strlen($i)) == $i) 
-        return TRUE;
+    if (substr($idx, 0, strlen($i)) == $i)
+      return TRUE;
 
     return FALSE;
   }
 
- /** \brief Tokenize string
-  *
-  * @param string (string)
-  * @return (array)
-  *
-  */
+  /** \brief Tokenize string
+   *
+   * @param string (string)
+   * @return (array)
+   *
+   */
 
   function tokenize($string) {
 
-  $tokens=preg_split($this->split_expression,$string, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $tokens=preg_split($this->split_expression,$string, -1, PREG_SPLIT_DELIM_CAPTURE);
 
-	if($this->case_insensitive) {
-		foreach($this->indexes as $k=>$v)	 { $this->indexes[$k]=strtolower($v); }
-		foreach($this->operators as $k=>$v)	 { $this->operators[$k]=strtolower($v); }
-	}
+    if ($this->case_insensitive) {
+      foreach($this->indexes as $k=>$v)	 {
+        $this->indexes[$k]=strtolower($v);
+      }
+      foreach($this->operators as $k=>$v)	 {
+        $this->operators[$k]=strtolower($v);
+      }
+    }
 
-   foreach($tokens as $k=>$v) {
-     if (empty($spos) && $v[0] == '"' && substr_count($v, '"') == 1)
-       $spos = $k;
-     elseif (isset($spos)) {
-       $tokens[$spos] .= $v;
-       if (strpos(' '.$v, '"')) unset($spos);
-       unset($tokens[$k]);
-     }
-     $last_token_index=$k;
-   }
+    foreach($tokens as $k=>$v) {
+      if (empty($spos) && $v[0] == '"' && substr_count($v, '"') == 1)
+        $spos = $k;
+      elseif (isset($spos)) {
+        $tokens[$spos] .= $v;
+        if (strpos(' '.$v, '"')) unset($spos);
+        unset($tokens[$k]);
+      }
+      $last_token_index=$k;
+    }
 
-   //Read a token
-   foreach($tokens as $k=>$v) {
-			$token=array();
+    //Read a token
+    foreach($tokens as $k=>$v) {
+      $token=array();
 
-	  //If the token is a index token
-     if($this->is_index($v)) {
-				$token['type']='INDEX';
+      //If the token is a index token
+      if ($this->is_index($v)) {
+        $token['type']='INDEX';
         if ($this->is_raw_index($v)) {
-				  $token['value']='_query_:"{!raw f=' . $v . '}';
+          $token['value']='_query_:"{!raw f=' . $v . '}';
           $use_raw = TRUE;
-        } else 
-				  $token['value']=$v;
-     
-			} else if($this->is_operator($v)) {
-				$token['type']='OPERATOR';
-        if ($use_raw && $v == '=')
-				  $token['value']='';
+        }
         else
-				  $token['value']=$v;
+          $token['value']=$v;
 
-			} else {
+      }
+      else if ($this->is_operator($v)) {
+        $token['type']='OPERATOR';
+        if ($use_raw && $v == '=')
+          $token['value']='';
+        else
+          $token['value']=$v;
 
-				$ignore=FALSE;
+      }
+      else {
 
-				foreach($this->ignore as $ign) {
-					if(preg_match($ign, $v)) {
-						$ignore=TRUE;
-					}
-				}
+        $ignore=FALSE;
 
-				if(!$ignore) {
-					$token['type']='OPERAND';
+        foreach($this->ignore as $ign) {
+          if (preg_match($ign, $v)) {
+            $ignore=TRUE;
+          }
+        }
+
+        if (!$ignore) {
+          $token['type']='OPERAND';
           if ($use_raw) {
-					  $token['value']=str_replace('"', '', $v) . '"';
-					  $token['raw_index']=TRUE;
+            $token['value']=str_replace('"', '', $v) . '"';
+            $token['raw_index']=TRUE;
             $use_raw = FALSE;
-          } else
-					  $token['value']=$v;
-				} 
-			}
+          }
+          else
+            $token['value']=$v;
+        }
+      }
 
-		if(!empty($token)) $tokenlist[]=$token;
+      if (!empty($token)) $tokenlist[]=$token;
 
-		}
-	return $tokenlist;
-	}
+    }
+    return $tokenlist;
+  }
 
 }
 ?>
