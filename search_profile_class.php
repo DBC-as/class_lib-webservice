be63764c5c460f4837203ce3797ebd13e6bcd392
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2012-01-19 10:34:38 +0000

    astyle --style=kr -s2 -S -y
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@37424 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- search_profile_class.php
+++ search_profile_class.php
@@ -33,71 +33,74 @@ require_once('OLS_class_lib/curl_class.php');
 
 class search_profiles {
 
-    private $profile_cache;		    // cache object
-    private $profiles;		        // profiles for $agency
-    private $agency_uri;	        // uri of openagency service
+  private $profile_cache;		    // cache object
+  private $profiles;		        // profiles for $agency
+  private $agency_uri;	        // uri of openagency service
 
-    public function __construct($open_agency, $cache_host, $cache_port='', $cache_seconds = 0) {
-        if ($cache_host) {
-            $this->profile_cache = new cache($cache_host, $cache_port, $cache_seconds);
-        }
-        $this->agency_uri = $open_agency;
+  public function __construct($open_agency, $cache_host, $cache_port='', $cache_seconds = 0) {
+    if ($cache_host) {
+      $this->profile_cache = new cache($cache_host, $cache_port, $cache_seconds);
+    }
+    $this->agency_uri = $open_agency;
+  }
+
+  /**
+  * \brief Get a given profile for the agency
+  *
+  * @param $profile_name       name of profile
+  *
+  * @returns profile if found, FALSE otherwise
+  **/
+  public function get_profile($agency, $profile_name) {
+    if ($this->profile_cache) {
+      $cache_key = 'PROFILE_' . $agency;
+      $this->profiles = $this->profile_cache->get($cache_key);
     }
 
-    /**
-    * \brief Get a given profile for the agency
-    *
-    * @param $profile_name       name of profile
-    *
-    * @returns profile if found, FALSE otherwise
-    **/
-    public function get_profile($agency, $profile_name) {
-        if ($this->profile_cache) {
-            $cache_key = 'PROFILE_' . $agency;
-            $this->profiles = $this->profile_cache->get($cache_key);
-        }
-
-        if (!$this->profiles) {
-            $curl = new curl();
-            $curl->set_option(CURLOPT_TIMEOUT, 10);
-            $res_xml = $curl->get(sprintf($this->agency_uri, $agency));
-            $curl_err = $curl->get_status();
-            if ($curl_err['http_code'] < 200 || $curl_err['http_code'] > 299) {
-                $this->profiles[strtolower($p_name)] = FALSE;
-                if (method_exists('verbose','log'))
-                  verbose::log(FATAL, __FUNCTION__ . '():: Cannot fetch profile ' . $profile_name . 
-                                        ' from ' . sprintf($this->agency_uri, $agency));
-            } else {
-                $dom = new DomDocument();
-                $dom->preserveWhiteSpace = false;
-                if (@ $dom->loadXML($res_xml)) {
-                    foreach ($dom->getElementsByTagName('profile') as $profile) {
-                        $p_name = '';
-                        $p_val = array();
-                        foreach ($profile->childNodes as $p)
-                            if ($p->localName == 'profileName')
-                                $p_name = $p->nodeValue;
-                            elseif ($p->localName == 'source') {
-                                foreach ($p->childNodes as $s)
-                                    $r[$s->localName] = $s->nodeValue;
-                                $p_val[] = $r;
-                            }
-                        $this->profiles[strtolower($p_name)] = $p_val;
-                    }
-                } else {
-                    $this->profiles = array();
-                }
-
+    if (!$this->profiles) {
+      $curl = new curl();
+      $curl->set_option(CURLOPT_TIMEOUT, 10);
+      $res_xml = $curl->get(sprintf($this->agency_uri, $agency));
+      $curl_err = $curl->get_status();
+      if ($curl_err['http_code'] < 200 || $curl_err['http_code'] > 299) {
+        $this->profiles[strtolower($p_name)] = FALSE;
+        if (method_exists('verbose','log'))
+          verbose::log(FATAL, __FUNCTION__ . '():: Cannot fetch profile ' . $profile_name .
+                       ' from ' . sprintf($this->agency_uri, $agency));
+      }
+      else {
+        $dom = new DomDocument();
+        $dom->preserveWhiteSpace = false;
+        if (@ $dom->loadXML($res_xml)) {
+          foreach ($dom->getElementsByTagName('profile') as $profile) {
+            $p_name = '';
+            $p_val = array();
+            foreach ($profile->childNodes as $p) {
+              if ($p->localName == 'profileName')
+                $p_name = $p->nodeValue;
+              elseif ($p->localName == 'source') {
+                foreach ($p->childNodes as $s)
+                $r[$s->localName] = $s->nodeValue;
+                $p_val[] = $r;
+              }
             }
-            if ($this->profile_cache)
-                $this->profile_cache->set($cache_key, $this->profiles);
+            $this->profiles[strtolower($p_name)] = $p_val;
+          }
+        }
+        else {
+          $this->profiles = array();
         }
 
-        if ($p = &$this->profiles[strtolower($profile_name)])
-            return $p;
-        else
-            return FALSE;
+      }
+      if ($this->profile_cache)
+        $this->profile_cache->set($cache_key, $this->profiles);
     }
+
+    if ($p = &$this->profiles[strtolower($profile_name)])
+      return $p;
+    else
+      return FALSE;
+  }
 
 }
 ?>
