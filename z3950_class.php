ddad3ad0c134d4532a6ed96b9cd7dcbb1c9b0702
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2013-02-13 11:35:16 +0000

    set_proxy()
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@54671 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- z3950_class.php
+++ z3950_class.php
@@ -176,6 +176,15 @@ class z3950 {
             $this->connect_options['otherInfo0'] = '1.2.840.10003.10.1000.81.3:' . $ip;
     }
 
+    /** \brief 
+     *
+     */
+    public function set_proxy($proxy, $cookie = '') {
+        $this->connect_options['proxy'] = $proxy;
+        if ($cookie)
+            $this->connect_options['cookie'] = $cookie;
+    }
+
     /** \brief set syntax
      *
      */
